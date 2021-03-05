c5106478464bdd3a8605035a6805bf0dcad0108e
h-arnold <78823477+h-arnold@users.noreply.github.com>
2021-03-05 16:27:05 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,12 @@
+// Makes the MiniBit turn 90 degress right.
 function right () {
     minibit.rotatems(mbRobotDirection.Right, 37, 350)
 }
+// Makes the MiniBit turn 90 degress left/
 function left () {
     minibit.rotatems(mbRobotDirection.Left, 37, 350)
 }
+// The main sequence that navigates the MiniBit through your maze.
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 2; index++) {
         forward()
@@ -11,10 +14,12 @@ input.onButtonPressed(Button.A, function () {
     left()
     forward()
 })
+// Allows you to stop the mini bit if you need to.
 input.onButtonPressed(Button.B, function () {
     minibit.stop(mbStopMode.Brake)
 })
 function forward () {
+    // This ensures that the MiniBit goes in a straight line
     minibit.mbBias(mbRobotDirection.Left, 10)
     minibit.goms(mbDirection.Forward, 60, 1000)
 }
